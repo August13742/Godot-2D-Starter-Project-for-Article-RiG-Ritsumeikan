2d14a90ed8a049e62a72381402099a20bf9a13ea
August13742 <149452958+August13742@users.noreply.github.com>
2025-05-31 05:00:22 +0900

    basic UI section completed
    next is audio

--- event_system.gd
+++ event_system.gd
@@ -9,3 +9,17 @@ func emit_trigger_camera_shake():
 func trigger_red_screen() -> Node:
 	var red_screen:CanvasLayer = load("uid://bwfy27hy7pu3p").instantiate()
 	return red_screen
+
+
+
+## ======END MENU HANDLING
+var end_menu_scene:PackedScene = preload("uid://nb5plvk2y6dj")
+func player_died()->void:
+	var end_menu:EndMenu = end_menu_scene.instantiate()
+	get_tree().get_current_scene().add_child(end_menu)
+	end_menu.is_victory = false
+	
+func slime_died()->void:
+	var end_menu:EndMenu = end_menu_scene.instantiate()
+	get_tree().get_current_scene().add_child(end_menu)
+	end_menu.is_victory = true
---
+++ player/bullet.gd
@@ -0,0 +1,35 @@
+extends Node2D
+
+
+@export var damage:int = 2
+@export var projectile_speed:float = 2000
+@export var direction:Vector2 = Vector2.RIGHT
+
+@onready var hitbox_component: Area2D = $HitboxComponent
+@onready var sprite: Sprite2D = $Bullet1
+@onready var visible_on_screen_notifier_2d: VisibleOnScreenNotifier2D = $VisibleOnScreenNotifier2D
+
+
+
+var current_speed:float = 0
+func _ready() -> void:
+	bullet_tween.call_deferred()
+	visible_on_screen_notifier_2d.screen_exited.connect(queue_free)
+
+	self.hitbox_component.damage = damage
+	hitbox_component.area_entered.connect(on_area_entered)
+func bullet_tween()->void:
+	var tween:Tween = create_tween()
+	tween.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BACK)
+	tween.tween_method(
+		func(value:float): current_speed = value,
+		0,
+		projectile_speed,
+		0.75)
+
+func _physics_process(delta: float) -> void:
+	self.position += direction * current_speed * delta
+
+func on_area_entered(other:Node2D):
+	if other is HurtboxComponent:
+		queue_free.call_deferred()
